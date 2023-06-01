115ca0bd6fb68b8cc1f13c53010d963dcdeaf64b
Mohaiman Mohammad Abdul <mohaiman@Mohaimans-MacBook-Air.local>
2023-06-01 07:25:34 +0600

    material ui added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
-
+import { Container , AppBar , Typography , Grow , Grid } from '@material-ui/core';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <Container maxWidth="lg">
+      <AppBar color='inherit' position='static'>
+        <Typography variant='h2' align='center'>
+          PicTrek
+          <img src='' alt='pictrek logo' height='60'></img>
+        </Typography>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify='space-between' alignItems='stretch' spacing={4}>
+            <Grid item xs={12} sm={7}>
+              <Posts></Posts>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form></Form>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
---
+++ src/components/Posts/Post/Post.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <div>This is Post</div>
+  )
+}
+
+export default Post
